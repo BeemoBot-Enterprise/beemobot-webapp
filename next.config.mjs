7216d1d1754c0013c4a2a89052ba48abaa2a29d9
jeremy-dura <jeremy.dura@homeserve.fr>
2026-01-21 12:52:22 +0100

    fix header

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,19 @@ const nextConfig = {
     BOT_INVITE_URL:
       "https://discord.com/oauth2/authorize?client_id=1316056047936471133&permissions=8&integration_type=0&scope=bot",
   },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "ngrok-skip-browser-warning",
+            value: "true",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
