b915659c1a825c4663d5dd8179ba74c645d08368
jeremy-dura <jeremy.dura@homeserve.fr>
2026-01-21 13:28:33 +0100

    fix url backend

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   },
   // Environment variables will be accessed through process.env instead of astro:env
   env: {
-    API_URL: "https://8818cad887f0.ngrok-free.app",
+    API_URL: "https://fb8ff02b18f6.ngrok-free.app",
     BOT_INVITE_URL:
       "https://discord.com/oauth2/authorize?client_id=1316056047936471133&permissions=8&integration_type=0&scope=bot",
   },
