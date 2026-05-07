982541307daf1bd14e40d17756f7e506d7d93eaf
Dura Jeremy <dura.jeremy@proton.me>
2026-05-07 17:31:37 +0200

    feat(webapp): rewrite landing page with Honey Friendly sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,29 +2,26 @@
  * Copyright (c) 2024-2026 BeemoBot Enterprise
  * All rights reserved.
  */
+import {
+  HeroLanding,
+  StatsLanding,
+  FeaturesLanding,
+  IntegrationsLanding,
+  LeaderboardTeaserLanding,
+  FaqLanding,
+  CtaLanding,
+} from "@/components/_design/landing";
 
-"use client";
-
-import HeroSection from "@/components/organisms/HeroSection";
-import { StatsSection } from "@/components/organisms/StatsSection";
-import { FeatureShowcase } from "@/components/organisms/FeatureShowcase";
-import { IntegrationsSection } from "@/components/organisms/IntegrationsSection";
-import LeaderboardTeaser from "@/components/organisms/LeaderboardTeaser";
-import { TeamSection } from "@/components/organisms/TeamSection";
-import { FaqSection } from "@/components/organisms/FaqSection";
-import { CTASection } from "@/components/organisms/CTASection";
-
-export default function Home() {
+export default function HomePage() {
   return (
     <>
-      <HeroSection />
-      <StatsSection />
-      <FeatureShowcase />
-      <IntegrationsSection />
-      <LeaderboardTeaser />
-      <TeamSection />
-      <CTASection />
-      <FaqSection />
+      <HeroLanding />
+      <StatsLanding />
+      <FeaturesLanding />
+      <IntegrationsLanding />
+      <LeaderboardTeaserLanding />
+      <FaqLanding />
+      <CtaLanding />
     </>
   );
 }
